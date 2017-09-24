07377adf7eb85cd217e364eea908a278e8d6d618
RickButler <rick@rickbutler.com>
2017-09-24 16:11:51 -0400

    webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,34 @@
 const path = require('path');
+const environment = process.env.NODE_ENV;
+const configs {
+	dev: {},
+	qa: {},
+	prod: {}
+}
 
-module.exports = {
-	entry: './src/index.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
-	}
+module.exports = (env = {}) => {
+	return {
+		entry: [
+			'babel-polyfill',
+			'./src/index.js',
+		],
+		output: {
+			filename: 'bundle.js',
+			path: path.resolve(__dirname, 'dist')
+		},
+		devtool: 'source-map',
+		module: {
+			rules: [{
+				test: /\.less$/,
+				use: [{
+					loader: "style-loader"
+				}, { 
+					loader: "css-loader"
+				},{
+					loader: "less-loader",
+						options: { sourceMap: true }
+					}]
+				}]
+			}
+		}
 };
